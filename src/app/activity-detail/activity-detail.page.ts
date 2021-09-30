d0dbb71951d7fbd4bf43e58c9fe142f4842df323
alyaa <alyaa@nitcotek.local>
2021-09-30 15:42:50 +0200

    new

--- src/app/activity-detail/activity-detail.page.ts
+++ src/app/activity-detail/activity-detail.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Activity } from '../models/activity';
+import { ActivityService } from '../services/Activity/activity.service';
 
 @Component({
   selector: 'app-activity-detail',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityDetailPage implements OnInit {
 
-  constructor() { }
+  activityDetail: Observable<Activity>;
+  activityID: string;
+
+  constructor(activityService: ActivityService,activatedRoute: ActivatedRoute) {
+    const activityID = activatedRoute.snapshot.params["activityID"];
+    console.log(activityID);
+    this.activityDetail=activityService.getActivity(activityID);
+    console.log(this.activityDetail)
+   }
 
   ngOnInit() {
+    
   }
 
 }
