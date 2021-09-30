8b322904e9b54bc5678e7bef4ebb35a4a1d42bfd
alyaa <alyaa@nitcotek.local>
2021-09-30 17:53:15 +0200

    new

--- src/app/activity-detail/activity-detail.page.ts
+++ src/app/activity-detail/activity-detail.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ModalController } from '@ionic/angular';
 import { Observable } from 'rxjs';
+import { ActivityVideoPage } from '../activity-video/activity-video.page';
 import { Activity } from '../models/activity';
 import { ActivityService } from '../services/Activity/activity.service';
 
@@ -14,7 +16,10 @@ export class ActivityDetailPage implements OnInit {
   activityDetail: Observable<Activity>;
   activityID: string;
 
-  constructor(activityService: ActivityService,activatedRoute: ActivatedRoute) {
+  constructor(
+    activityService: ActivityService,
+    activatedRoute: ActivatedRoute,
+    private _modalController: ModalController) {
     const activityID = activatedRoute.snapshot.params["activityID"];
     console.log(activityID);
     this.activityDetail=activityService.getActivity(activityID);
@@ -23,6 +28,25 @@ export class ActivityDetailPage implements OnInit {
 
   ngOnInit() {
     
+  }
+
+  async openModal(){
+    console.log('00')
+    const videoModal= await this._modalController.create({
+      component:ActivityVideoPage
+    });
+
+  return  await this.activityDetail.subscribe((activity)=>{
+      videoModal.componentProps={
+        videoURL: activity.video_url
+      }
+      return  videoModal.present();
+    });
+
+      console.log(videoModal.componentProps)
+
+   
+
   }
 
 }
